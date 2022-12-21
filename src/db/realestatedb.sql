25957ddca2cc01a6fce68bf585b9551114bcd79c
Vic <89066365+Beeecc@users.noreply.github.com>
2022-12-21 13:20:06 +0800

    new sql

--- src/db/realestatedb.sql
+++ src/db/realestatedb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2022 at 07:17 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
+-- Generation Time: Dec 21, 2022 at 06:19 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,7 @@ CREATE TABLE `admin_t` (
   `address` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
   `manager_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -48,7 +48,14 @@ CREATE TABLE `apartment_t` (
   `rental_fee` decimal(10,2) NOT NULL,
   `availablility` enum('vacant','occupied') NOT NULL,
   `manager_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `apartment_t`
+--
+
+INSERT INTO `apartment_t` (`apartment_no`, `rental_fee`, `availablility`, `manager_id`) VALUES
+(1, '500.00', 'vacant', 1);
 
 -- --------------------------------------------------------
 
@@ -61,7 +68,17 @@ CREATE TABLE `complaint_t` (
   `date_received` datetime NOT NULL,
   `complaint` varchar(255) NOT NULL,
   `tenant_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `complaint_t`
+--
+
+INSERT INTO `complaint_t` (`complaint_id`, `date_received`, `complaint`, `tenant_id`) VALUES
+(1, '2022-12-21 01:44:59', 'the dsdssdsdsd broken', 1),
+(2, '2022-12-21 01:46:27', 'bsdbuabdadas', 2),
+(3, '2022-12-21 01:46:47', 'sdsdadsdasdas', 1),
+(4, '2022-12-21 05:43:34', 'the barbrbabrar', 1);
 
 -- --------------------------------------------------------
 
@@ -75,7 +92,14 @@ CREATE TABLE `manager_t` (
   `lname` varchar(16) NOT NULL,
   `sex` enum('male','female') NOT NULL,
   `contact_no` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manager_t`
+--
+
+INSERT INTO `manager_t` (`manager_id`, `fname`, `lname`, `sex`, `contact_no`) VALUES
+(1, 'vic', 'paro', 'male', '12');
 
 -- --------------------------------------------------------
 
@@ -84,11 +108,19 @@ CREATE TABLE `manager_t` (
 --
 
 CREATE TABLE `payment_t` (
-  `tenant_id` int(11) NOT NULL,
+  `payment_id` int(11) NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `payment_method` enum('online payment','check','cash') NOT NULL,
   `date_paid` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payment_t`
+--
+
+INSERT INTO `payment_t` (`payment_id`, `amount`, `payment_method`, `date_paid`) VALUES
+(1, '500.69', 'online payment', '2022-12-21'),
+(2, '1000.00', 'cash', '2022-12-21');
 
 -- --------------------------------------------------------
 
@@ -102,7 +134,7 @@ CREATE TABLE `repairman_t` (
   `lname` varchar(16) NOT NULL,
   `schedule` datetime NOT NULL,
   `manager_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -120,7 +152,34 @@ CREATE TABLE `tenant_t` (
   `apartment_no` int(11) NOT NULL,
   `contract_start` date NOT NULL,
   `contract_end` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tenant_t`
+--
+
+INSERT INTO `tenant_t` (`tenant_id`, `fname`, `lname`, `sex`, `contact_no`, `address`, `apartment_no`, `contract_start`, `contract_end`) VALUES
+(1, 'civ', 'civ', 'male', '12', 'cebu', 1, '2022-12-21', '2022-12-28'),
+(2, 'v', 'par', 'male', '0912', 'cebu', 1, '2022-12-21', '2022-12-28'),
+(3, 'Vic', 'Parojinog', 'male', '0909123123', 'Lahug', 1, '2022-12-21', '2022-12-30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_t`
+--
+
+CREATE TABLE `user_t` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_t`
+--
+
+INSERT INTO `user_t` (`username`, `password`) VALUES
+('vicedgar', '123456');
 
 --
 -- Indexes for dumped tables
@@ -157,7 +216,7 @@ ALTER TABLE `manager_t`
 -- Indexes for table `payment_t`
 --
 ALTER TABLE `payment_t`
-  ADD PRIMARY KEY (`tenant_id`);
+  ADD PRIMARY KEY (`payment_id`);
 
 --
 -- Indexes for table `repairman_t`
@@ -187,25 +246,25 @@ ALTER TABLE `admin_t`
 -- AUTO_INCREMENT for table `apartment_t`
 --
 ALTER TABLE `apartment_t`
-  MODIFY `apartment_no` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `apartment_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `complaint_t`
 --
 ALTER TABLE `complaint_t`
-  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `manager_t`
 --
 ALTER TABLE `manager_t`
-  MODIFY `manager_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `manager_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `payment_t`
 --
 ALTER TABLE `payment_t`
-  MODIFY `tenant_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `repairman_t`
@@ -217,7 +276,7 @@ ALTER TABLE `repairman_t`
 -- AUTO_INCREMENT for table `tenant_t`
 --
 ALTER TABLE `tenant_t`
-  MODIFY `tenant_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `tenant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -245,7 +304,7 @@ ALTER TABLE `complaint_t`
 -- Constraints for table `payment_t`
 --
 ALTER TABLE `payment_t`
-  ADD CONSTRAINT `paymentFK1` FOREIGN KEY (`tenant_id`) REFERENCES `tenant_t` (`tenant_id`);
+  ADD CONSTRAINT `paymentFK1` FOREIGN KEY (`payment_id`) REFERENCES `tenant_t` (`tenant_id`);
 
 --
 -- Constraints for table `repairman_t`
