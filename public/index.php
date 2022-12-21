a06c997fc785fb66452e634f5cb2361325dbb8be
Adrian Albino <21102558@usc.edu.ph>
2022-12-21 09:26:20 +0800

    Payment history backend

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<?php 
+    include "../src/php/db_connect.php"
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -53,7 +56,7 @@
                         </ul>
                     </div>
                 </nav>
-                <div class="container-fluid px-4">
+                <!--<div class="container-fluid px-4">
                     <div class="row g-3 my-2">
                         <div class="col-md-3">
                             <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
@@ -92,92 +95,50 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row my-5">
-                        <h3 class="fs-4 mb-3">Recent Orders</h3>
-                        <div class="col">
-                            <table class="table bg-white rounded shadow-sm  table-hover">
-                                <thead>
-                                    <tr>
-                                        <th scope="col" width="50">#</th>
-                                        <th scope="col">Product</th>
-                                        <th scope="col">Customer</th>
-                                        <th scope="col">Price</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
+                     <tbody>
                                     <tr>
                                         <th scope="row">1</th>
                                         <td>Television</td>
                                         <td>Jonny</td>
                                         <td>$1200</td>
-                                    </tr>
+                                    </tr>-->
+                    <div class="row my-5">
+                        <h3 class="fs-4 mb-3">Payments</h3>
+                        <div class="col">
+                            <table class="table bg-white rounded shadow-sm  table-hover">
+                                <thead>
                                     <tr>
-                                        <th scope="row">2</th>
-                                        <td>Laptop</td>
-                                        <td>Kenny</td>
-                                        <td>$750</td>
+                                        <th scope="col">First Name</th>
+                                        <th scope="col">Last Name</th>
+                                        <th scope="col">Amount</th>
+                                        <th scope="col">Date Paid</th>
                                     </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Cell Phone</td>
-                                        <td>Jenny</td>
-                                        <td>$600</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">4</th>
-                                        <td>Fridge</td>
-                                        <td>Killy</td>
-                                        <td>$300</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">5</th>
-                                        <td>Books</td>
-                                        <td>Filly</td>
-                                        <td>$120</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">6</th>
-                                        <td>Gold</td>
-                                        <td>Bumbo</td>
-                                        <td>$1800</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">7</th>
-                                        <td>Pen</td>
-                                        <td>Bilbo</td>
-                                        <td>$75</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">8</th>
-                                        <td>Notebook</td>
-                                        <td>Frodo</td>
-                                        <td>$36</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">9</th>
-                                        <td>Dress</td>
-                                        <td>Kimo</td>
-                                        <td>$255</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">10</th>
-                                        <td>Paint</td>
-                                        <td>Zico</td>
-                                        <td>$434</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">11</th>
-                                        <td>Carpet</td>
-                                        <td>Jeco</td>
-                                        <td>$1236</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">12</th>
-                                        <td>Food</td>
-                                        <td>Haso</td>
-                                        <td>$422</td>
-                                    </tr>
-                                </tbody>
+                                </thead>
+                                <?php
+                                    // display data from database in tables
+                                    $sql = "SELECT t.fname, t.lname, p.amount, p.date_paid
+                                    FROM tenant_t t,
+                                         payment_t p
+                                    WHERE t.tenant_id = p.tenant_id
+                                    ORDER BY c.date_received DESC;";
+                                    $result = mysqli_query($con, $sql);
+                                    if (mysqli_num_rows($result) > 0) {
+                                    while($row = mysqli_fetch_assoc($result)) {
+                                    ?>
+
+                                    <tbody>
+                                    <tr><td><?php echo $row['fname']?></td>
+                                    <td><?php echo $row['lname']?></td>
+                                    <td><?php echo $row['amount']?></td>
+                                    <td><?php echo $row['date_paid']?></td>
+                                    </tbody>
+                                    <?php 
+                                    }
+                                    } else {
+                                        echo "0 results";
+                                    }
+                                    mysqli_close($con);
+                                    ?>
                             </table>
                         </div>
                     </div>
