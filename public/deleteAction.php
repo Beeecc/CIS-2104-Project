3b8e9b1a70146575e64a0f952119cbfd466476b8
Adrian Albino <21102558@usc.edu.ph>
2022-12-21 15:25:53 +0800

    alert test

--- public/deleteAction.php
+++ public/deleteAction.php
@@ -7,19 +7,21 @@ if(isset($_GET['Del']))
     $ID = $_GET['Del'];
     $sql = "DELETE FROM complaint_t WHERE tenant_id = '".$ID."';";
     $result = mysqli_query($con, $sql);
-    echo "<script type='text/javascript'>alert($result);</script>";
     if($result)
     {
         header("location:complaints_log.php");
+        echo "<script type='text/javascript'>alert('Should work');</script>";
     }
     else
     {
         echo 'Please check your query.';
+        echo "<script type='text/javascript'>alert('Something wrong');</script>";
     }
 }
 else
 {
     header("location:complaints_log.php");
+    echo "<script type='text/javascript'>alert('Didn't get DEL');</script>";
 }
 
 ?>
